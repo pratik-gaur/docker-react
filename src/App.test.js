511f1e56eed66354a301be4508ecc3b338dd191b
pratik.gaur <pratik.gaur@olx.com>
2023-03-16 15:33:23 +0100

    initial commit 1

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,11 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders learn react link 1', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
+
+
